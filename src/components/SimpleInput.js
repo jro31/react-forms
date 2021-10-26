8598f0e0b2328b6e70d506bcd91a548db7b5cf84
Jethro Williams <jethrowilliams@outlook.com>
2021-10-26 11:28:26 +0100

    Add simple validation onBlur (when the user clicks out of the input)
    As well as remove the useRef code

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,18 +1,24 @@
-// useRef vs useState for logging input values:
-// Use useRef if you're only interested in the value once, when the form is submitted. No need to log the value on every keystroke in this case.
-// Use useState if you need the entered value after every keystroke (for example, for validation), because with useRef you can't really do that.
-// You can also reset the input easily when using state, by binding the 'value' prop on the input to the 'enteredName' state (or other state) as below
-// (doing this with useRef, you have to manipulate the DOM directly (not through React), in which case you're not going to have a good time)
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
   const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
+
+    if (event.target.value.trim() !== '') {
+      setEnteredNameIsValid(true);
+    };
+  };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+    };
   };
 
   const formSumbissionHandler = (event) => {
@@ -29,9 +35,6 @@ const SimpleInput = (props) => {
 
     console.log(enteredName);
 
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
-
     setEnteredName('');
   };
 
@@ -42,9 +45,17 @@ const SimpleInput = (props) => {
   return (
     <form onSubmit={formSumbissionHandler}>
       <div className={nameInputClasses}>
-        <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
-        {nameInputIsInvalid && <p className='error-text'>Name must not be empty</p>}
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
